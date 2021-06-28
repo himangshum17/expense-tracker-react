d71a050f00d2d9fddd4833d72b1d11fb58aaea1a
Himangshu Mondal <mailtoherock@gmail.com>
2021-06-28 23:32:01 +0530

    Add new expense form updated

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,7 +1,37 @@
+import { useState } from 'react';
 const ExpenseForm = () => {
+  const [enteredTitle, setEnteredTitle] = useState('');
+  const [enteredAmount, setEnteredAmount] = useState('');
+  const [enteredDate, setEnteredDate] = useState('');
+  const changeTitleHandler = (e) => {
+    setEnteredTitle(e.target.value);
+  };
+  const changeAmountHandler = (e) => {
+    setEnteredAmount(e.target.value);
+  };
+  const changeDateHandler = (e) => {
+    setEnteredDate(e.target.value);
+  };
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+
+    const expanseData = {
+      title: enteredTitle,
+      amount: enteredAmount,
+      date: new Date(enteredDate),
+    };
+    setEnteredTitle('');
+    setEnteredAmount('');
+    setEnteredDate('');
+  };
+
   return (
     <>
-      <form className='grid grid-cols-4 gap-4 items-end'>
+      <form
+        className='grid grid-cols-4 gap-4 items-end'
+        onSubmit={submitHandler}
+      >
         <div className='flex flex-col'>
           <label htmlFor='title' className='text-gray-200 font-medium'>
             Title
@@ -11,6 +41,8 @@ const ExpenseForm = () => {
             type='text'
             name='title'
             id='title'
+            onChange={changeTitleHandler}
+            value={enteredTitle}
           />
         </div>
         <div className='flex flex-col'>
@@ -24,6 +56,8 @@ const ExpenseForm = () => {
             step='0.01'
             name='amount'
             id='amount'
+            onChange={changeAmountHandler}
+            value={enteredAmount}
           />
         </div>
         <div className='flex flex-col'>
@@ -35,6 +69,8 @@ const ExpenseForm = () => {
             type='date'
             name='date'
             id='date'
+            onChange={changeDateHandler}
+            value={enteredDate}
           />
         </div>
         <div>
